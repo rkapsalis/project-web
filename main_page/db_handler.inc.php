fe4407262ee0a5ab180e649f2b5af24f16e5338c
rkapsalis <48930309+rkapsalis@users.noreply.github.com>
2020-02-05 00:26:59 +0200

    Create db_handler.inc.php

---
+++ main_page/db_handler.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServerName = "localhost";
+$dbUserName = "root";
+$dbPassowrd = "";
+$dbName = "web";
+
+//Variable that holds connection to database
+$conn = mysqli_connect($dbServerName, $dbUserName, $dbPassowrd, $dbName);
+?>
