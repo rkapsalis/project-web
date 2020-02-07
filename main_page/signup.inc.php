1dc5f304fe351ac7338b53274e45ec2a634f2642
rkapsalis <48930309+rkapsalis@users.noreply.github.com>
2020-02-07 19:42:18 +0200

    Update signup.inc.php

--- main_page/signup.inc.php
+++ main_page/signup.inc.php
@@ -1,36 +1,36 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-//if(isset($_POST["signup"])){
+if(isset($_POST["pwd_repeat"])){
     require_once 'db_handler.inc.php';
-   
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    $passwordr = $_POST["passwordr"];
-    $email = $_POST["email"];
-
+    
+    $username = $_POST["usr"];
+    $password = $_POST["pwd"];
+    $passwordr = $_POST["pwd_repeat"];
+    $email = $_POST["mail"];
+    
     if(strlen($password)<8 ){
-      header("Location:.../index.html?error=shortPassword");
-	  
+	  echo "$password";
+	  //header("Location:.../main.html?error=shortPassword");
       exit();
     }
     elseif (!preg_match("/[A-Z]/", $password)) { //at least one capital letter
-        header("Location:.../index.html?error=atLeastOneCapital");
+        header("Location:.../www/main.html?error=atLeastOneCapital");
         exit();
     }
     elseif (!preg_match("/\W/", $password)) { //at least one special character
-        header("Location:.../index.html?error=atLeastOneSymbol");
+        header("Location:.../main.html?error=atLeastOneSymbol");
         exit();
     }
     elseif (!preg_match("/\d/", $password)) { //at least one digit
-        header("Location:.../index.html?error=atLeastOneDigit");
+        header("Location:.../main.html?error=atLeastOneDigit");
         exit();
     }
     elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)){ //δεν το ζηταει,ισως χρειαζεται
-        header("Location:.../index.html?error=");
+        header("Location:.../main.html?error=");
         exit();
     }
     elseif ($password !== $passwordr){
-        header("Location:.../index.html?error=passwordsNotMatch");
+        header("Location:.../main.html?error=passwordsNotMatch");
         exit();
     }
     else{
@@ -45,24 +45,28 @@ header("Access-Control-Allow-Origin: *");
         //password hashing
         $hashedPass = password_hash($password, PASSWORD_DEFAULT);
 
-        $sql = "INSERT INTO user(username, password,email) VALUES (?,?,?)";
+        $sql = "INSERT INTO user(username,password,email) VALUES (?,?,?)";
         $stmt = mysqli_stmt_init($conn);
         if(!mysqli_stmt_prepare($stmt,$sql)){
-			$sqlerror = mysqli_error($conn);
-            header("Location:.../index.html?error=".$sqlerror."");
-			echo '$hashedPass';
+            //header("Location:.../main.html?error=sqlerror");
+			//echo 'sth';
+			//echo("Error description: " . mysqli_error($conn));
             exit();
         }
         else{
             mysqli_stmt_bind_param($stmt,"sss",$username,$hashedPass,$email);
             mysqli_stmt_execute($stmt);
             mysqli_stmt_store_result($stmt);
-            header("Location:.../index.html?signup=success");
+            //header("Location:.../main.html?signup=success");
+			echo 1; 
             exit();
+			
         }
-        echo ("success") ;        
+               
     }
     //θα βαλουμε να τσεκαρει και αν υπαρχει ηδη χρηστης στην βαση με το ιδιο username?
     mysqli_stmt_close($stmt);
     mysqli_close($conn);
-//}
+
+}
+?>
