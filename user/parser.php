6de92ac2e19c6f902ac0f6e835a493885d2770f9
rkapsalis <48930309+rkapsalis@users.noreply.github.com>
2020-02-19 00:57:41 +0200

    Update parser.php

--- user/parser.php
+++ user/parser.php
@@ -28,6 +28,25 @@ $c=0;
 $activity_timestamp = "NULL";
 $activity_type = "NULL";
 $activity_confidence = "NULL";
+
+function withinPatras($latitudeTo, $longitudeTo)
+      {
+       
+        $earth_radius = 6371000;
+        $latitudeFrom = deg2rad(38.230462);
+        $longitudeFrom = deg2rad(21.753150);
+        $latitudeTo = deg2rad($latitudeTo);
+        $longitudeTo = deg2rad($longitudeTo);
+
+        $LatD = $latitudeTo - $latitudeFrom;  
+        $LonD = $longitudeTo - $longitudeFrom; 
+        
+        
+        $angle = 2 * asin(sqrt(pow(sin($LatD / 2), 2) + cos($latitudeFrom) * cos($latitudeTo) * pow(sin($LonD / 2), 2)));
+        return $angle * $earth_radius;
+        
+}
+
 foreach ($users as $row=>$value ) {
 	           //if($row == 'timestampMs'){
 	            $timestampMs = $value['timestampMs'];				
@@ -39,10 +58,15 @@ foreach ($users as $row=>$value ) {
 	            $longitudeE7 = $value['longitudeE7'];
 			  //}
 		    $accuracy = $value['accuracy'];
+	if(withinPatras($latitudeE7 / 10000000.0, $longitudeE7 / 10000000.0)<=10000){
 		    $arr[$count]["timestampMs"] = $timestampMs;
 		    $arr[$count]["latitudeE7"] = $latitudeE7;
 		    $arr[$count]["longitudeE7"] = $longitudeE7;
 		    $arr[$count]["accuracy"] = $accuracy;
+		    if (isset($arr[$count]["altitude"])) { $altitude = $arr[$count]["altitude"]; }
+		    if (isset($arr[$count]["verticalAccuracy"])) { $verticalAccuracy = $arr[$count]["verticalAccuracy"]; }
+		    if (isset($arr[$count]["velocity"])) { $velocity = $arr[$count]["velocity"]; }
+	            if (isset($arr[$count]["heading"])) { $heading = $arr[$count]["heading"]; }
 	            $count++;
 	              	if (isset($value["activity"])) {
 			 
@@ -53,16 +77,21 @@ foreach ($users as $row=>$value ) {
 				  $c=count($activity["activity"]);
 									   
 				  if($c>1 ){
+					  $temp_count = $counter;
 				     foreach ($valac["activity"] as $vala ){
 					
-					     $activity_type = $activity["activity"][$counter1]["type"];
-					     $activity_confidence = $activity["activity"][$counter1]["confidence"];
-					     $arr_ac[$counter]["timestampMs"] = $activity_timestamp;
-			                     $arr_ac[$counter][$counter1]["type"] = $activity_type;
-			                     $arr_ac[$counter][$counter1]["confidence"] = $activity_confidence;
-			                     $arr_ac[$counter][$counter1]["count"] = $count;
-					     $counter1++;
-				     }					   
+					   $activity_type = $activity["activity"][$counter1]["type"];
+					   $activity_confidence = $activity["activity"][$counter1]["confidence"];
+					   $arr_ac[$temp_count]["timestampMs"] = $activity_timestamp;
+					   $arr_ac[ $temp_count ]["nested"] = "yes";
+			                   $arr_ac[$temp_count]["type"] = $activity_type;
+			                   $arr_ac[$temp_count]["confidence"] = $activity_confidence;
+			                   $arr_ac[$temp_count]["count"] = $count;
+					   $arr_ac[$temp_count]["count_arr"] = $counter2;
+					   $counter1++;
+					   $temp_count++;
+				     }	
+			             $counter = $temp_count;
 				  }
 				 else{
 				     $activity_type = $activity["activity"][0]["type"];									   
@@ -80,7 +109,7 @@ foreach ($users as $row=>$value ) {
 				$counter2 = 0;
 		}	
 	 
-	
+	}
 }
 
 $inserter = new BulkInserter($pdo, 'data', ['timestampMs', 'latitudeE7', 'longitudeE7', 'accuracy']);
@@ -88,8 +117,11 @@ $pdo->beginTransaction();
 foreach ($arr as $value){
 	$inserter->queue( $value['timestampMs'] , $value['latitudeE7'] , $value['longitudeE7'] , $value['accuracy'] );   
 }
-
+foreach ($arr_ac as $value_ac){			 
+	$inserter2->queue( $value_ac['timestampMs'] , $value_ac['type'] , $value_ac['confidence'] , $value_ac['count'] , $value_ac['count_arr']);
+}
 $inserter->flush();
+$inserter2->flush();
 $pdo->commit();
 
 ?>
