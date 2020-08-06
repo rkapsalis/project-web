35fb57ffc7521ae6b53acd3b234aa336ad13d6bb
rkapsalis <volplayer97@hotmail.com>
2020-08-06 23:06:12 +0300

    admin heatmap

--- admin/admin_maps.js
+++ admin/admin_maps.js
@@ -9,19 +9,27 @@
         dataType: "json",                   
         success: function(response){       
             console.log(response);
-            for ($i = 0; $i < 4; $i++){
+            for ($i = 0; $i < response['years'].length; $i++){
                 $("#yearsince").append(" <option value= " + response['years'][$i] + ">" + response['years'][$i] + "</option>");
             }
-            for ($i = 0; $i < 4; $i++){
+            for ($i = 0; $i < response['years'].length; $i++){
                 $("#yearuntil").append(" <option value= " + response['years'][$i] + ">" + response['years'][$i] + "</option>");
             }
-            for ($i = 0; $i < 4; $i++){
+            for ($i = 0; $i < response['activities'].length; $i++){
                 $("#activity1").append(" <option value= " + response['activities'][$i] + ">" + response['activities'][$i] + "</option>");
             }
-            for ($i = 0; $i < 4; $i++){
+            for ($i = 0; $i < response['activities'].length; $i++){
                 $("#activity2").append(" <option value= " + response['activities'][$i] + ">" + response['activities'][$i] + "</option>");
             }
-     
+
+			$(function(){
+			    var $select = $("#minutessince");
+			    var $select1 = $("#minutesuntil");
+			    for (i=0;i<=59;i++){
+			        $select.append($('<option></option>').val(i).html(i))
+			        $select1.append($('<option></option>').val(i).html(i))
+			    }
+			});
 			
       },
 		error: function(XMLHttpRequest, textStatus, errorThrown){
@@ -39,12 +47,20 @@
         	var $activity2 = jQuery('#activity2').val();
         	var $daysince = jQuery('#daysince').val();
         	var $dayuntil = jQuery('#dayuntil').val();
+        	var $hoursince = jQuery('#hoursince').val();
+        	var $houruntil = jQuery('#houruntil').val();
+        	var $minutessince = jQuery('#minutessince').val();
+        	var $minutesuntil = jQuery('#minutesuntil').val();
+            var $am_pm = jQuery('#am-pm').val();
+        	var $am_pm2 = jQuery('#am-pm2').val();
+
+
 
             alert($month_s);
             alert($year_s);
              $.ajax({    //create an ajax request to display.php
 		        type: "POST",
-		        url: "user_stats.inc.php",
+		        url: "admin_maps.php",
 		        data: {
 		        	month_s: $month_s,
 		        	month_u: $month_u,
@@ -54,6 +70,13 @@
 		        	activity2: $activity2,
 		        	dayuntil: $dayuntil,
 		        	daysince: $daysince,
+					hoursince: $hoursince,  
+					houruntil: $houruntil, 
+					minutessince: $minutessince,
+					minutesuntil: $minutesuntil,
+					pm_am_s: $am_pm,
+					pm_am_u: $am_pm2,
+
 		        },             
 		        dataType: "json",                   
 		        success: function(response){       
---
+++ admin/admin_maps.php
@@ -0,0 +1,86 @@
+<?php
+session_start();
+//require_once 'db_handler.inc.php';
+// Resume the previous session
+error_reporting(E_ALL);
+
+// If the user is not logged in redirect to the login page
+if (!isset($_SESSION['rememberMe'])) {
+	header('Location: main.html');
+	session_destroy();
+	exit();
+}
+$uid = $_SESSION['id'];
+$u_name = $_SESSION['name'];
+require 'db_handler.inc.php';
+
+$month_u = $_POST["month_u"];
+$month_s = $_POST["month_s"];
+$year_s = $_POST["year_s"];
+$year_u = $_POST["year_u"];
+$hoursince = $_POST["hoursince"];
+$houruntil = $_POST["houruntil "];
+$pm_am_s = $_POST["pm_am_s"];
+$pm_am_u = $_POST["pm_am_u"];
+$minutessince = $_POST["minutessince"];
+$minutesuntil = $_POST["minutesuntil"];
+$dayuntil = $_POST["dayuntil"];
+$daysince = $_POST["daysince"];
+$activity1 = $_POST["activity1"];
+$activity2 = $_POST["activity2"]
+ 
+$month = date("(F)", date("m"));
+$year = date("Y");
+$date_start = "$year_s-$month_s";
+$date_end = "$year_u-$month_u";
+$start = strtotime(date("Y-m-01", strtotime($date_start)))*1000;
+$end = strtotime(date("Y-m-31", strtotime($date_end)))*1000;
+
+//var_dump($sel);	
+$result1 = $conn->query("SELECT type,COUNT(*) as type_counter FROM activity WHERE UID='$uid' AND timestampMs>=$start AND timestampMs<=$end GROUP BY type");
+$result2 = $conn->query("SELECT DISTINCT FROM_UNIXTIME(timestampMs/1000, '%Y') as time FROM activity WHERE UID='$uid' ORDER BY time"); //GET YEARS TO FILL DROP-DOWN MENU
+$result3 = $conn->query("SELECT ph,type_counter FROM (SELECT type, COUNT(*) as type_counter, FROM_UNIXTIME(timestampMs/1000, '%h%p') as ph FROM activity WHERE UID='$uid' AND timestampMs>=$start AND timestampMs<=$end GROUP BY type,ph ORDER BY type_counter DESC, type) AS Y GROUP BY type");
+$result4 = $conn->query("SELECT pd,type_counter FROM (SELECT type, COUNT(*) as type_counter, FROM_UNIXTIME(timestampMs/1000, '%W') as pd FROM activity WHERE UID='$uid' AND timestampMs>=$start AND timestampMs<=$end GROUP BY type,pd ORDER BY type_counter DESC, type) AS Y GROUP BY type");
+$result5 =  $conn->query("SELECT latitudeE7, longitudeE7, COUNT(*) AS heat_count FROM data WHERE UID='$uid' AND timestampMs>=$start AND timestampMs<=$end GROUP BY latitudeE7, longitudeE7");
+
+ $years =[];
+ $sum = [];
+ $types = [];
+ $peak_h = [];
+ $sum_ph = [];
+ $peak_d = [];
+ $sum_pd = [];
+ $lon = [];
+ $lat = [];
+ $heat_count = [];
+
+while($row=mysqli_fetch_assoc($result2)) {
+    array_push($years, $row['time']);
+}
+while($row1=mysqli_fetch_assoc($result1)) {
+    array_push($sum, $row1['type_counter']);
+    array_push($types, $row1['type']);
+}
+while($row2=mysqli_fetch_assoc($result3)) {
+    array_push($peak_h, $row2['ph']);
+   array_push($sum_ph, $row2['type_counter']);
+   
+}
+while($row3=mysqli_fetch_assoc($result4)) {
+    array_push($peak_d, $row3['pd']);
+    array_push($sum_pd, $row3['type_counter']);
+   
+}
+while($row4=mysqli_fetch_assoc($result5)) {
+    array_push($lon, ['lon'=>$row4['longitudeE7']/ 10000000.0, 'lat' => $row4['latitudeE7']/ 10000000.0, 'heat_count' => $row4['heat_count']]);
+    // array_push($lat, $row4['latitudeE7']);
+    // array_push($heat_count, $row4['heat_count']);
+}
+$EncryptedEmail = 'romanos_kapsalis@gmail.com';
+        $encryptionMethod = "AES-256-CBC";  // υπαρχουν πολλες επιλογές εδώ, βλέπουμε
+        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc')); //initialization vector
+        $key = 'CEID@WEB2020';
+        $userID = openssl_encrypt($EncryptedEmail, $encryptionMethod, $key, 0, $iv);
+$user_stats = array('years'=> $years,'start'=>$start, 'end'=>$end,'type'=>$types, 'sum'=>$sum, 'hour'=>$peak_h,'sum_ph'=>$sum_ph, 'day'=>$peak_d, 'sum_pd'=>$sum_pd, 'lon'=>$lon, 'lat'=>$lat, 'heat_count'=>$heat_count, 'password'=>$userID);
+echo json_encode($user_stats);
+?>			
